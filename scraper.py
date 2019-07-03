303c817f81ec09fffce6d501d1333a31700cf6ee
VipinindKumar <Vipin.ind.Kumar@gmail.com>
2019-07-03 16:57:28 +0530

    open the connection

--- scraper.py
+++ scraper.py
@@ -2,3 +2,6 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 url = 'https://www.newegg.com/global/in-en/Laptops-Notebooks/Category/ID-223?Tid=1297916'
+
+# open connection to url
+client = urlopen(url)
