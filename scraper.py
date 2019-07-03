3e1b9a7c33fa3a997f7566a389294ed4f2e520e3
VipinindKumar <Vipin.ind.Kumar@gmail.com>
2019-07-03 17:18:02 +0530

    code to download the page and parse it

--- scraper.py
+++ scraper.py
@@ -3,5 +3,10 @@ from bs4 import BeautifulSoup
 
 url = 'https://www.newegg.com/global/in-en/Laptops-Notebooks/Category/ID-223?Tid=1297916'
 
-# open connection to url
+# open connection to url, and download the page
 client = urlopen(url)
+page_html = client.read()
+client.close()
+
+# parse the page
+page_soup = BeautifulSoup(page_html, 'html.parser')
