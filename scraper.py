e094fa711d414e35a66679d5bdcb229e179c6f30
VipinindKumar <Vipin.ind.Kumar@gmail.com>
2019-07-04 17:09:55 +0530

    code to extract current price of the product

--- scraper.py
+++ scraper.py
@@ -21,9 +21,19 @@ f.write('Brand, Name\n')
 conts = page_soup.findAll('div', {'class': 'item-container'})
 
 for cont in conts:
+	# getting brand name from the title of img tag
 	brand = cont.find('div', 'item-info').div.a.img['title']
 
+	# getting title/name of the product
 	title = cont.findAll('a', {'class': 'item-title'})[0].text
+
+	# previous price
+	pw = cont.find('div', 'item-info').find('div','item-action').ul.li.span.text
+
+	# current price of the product
+	pc = cont.find('div', 'item-info').find('div','item-action').ul.find('li','price-current').text
+	pc = pc.replace(',', '')
+	pc = re.search('.+\s([0-9]+).+', pc).group(1)
 
 	f.write(brand + ',' + title.replace(',', ' ') + '\n')
 
