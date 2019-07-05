eaede0bce54a0f32f413e1a7e8bb7cc12b727fe3
VipinindKumar <Vipin.ind.Kumar@gmail.com>
2019-07-05 08:11:58 +0530

    fix pw and dc not available

--- scraper.py
+++ scraper.py
@@ -28,16 +28,23 @@ for cont in conts:
 	# getting title/name of the product
 	title = cont.findAll('a', {'class': 'item-title'})[0].text
 
-	# previous price
-	pw = cont.find('div', 'item-info').find('div','item-action').ul.li.span.text
-
+	# previous price, if available
+	try:
+		pw = cont.find('div', 'item-info').find('div','item-action').ul.li.span.text
+	except:
+		pw = 'NaN'
+	
 	# current price of the product
 	pc = cont.find('div', 'item-info').find('div','item-action').ul.find('li','price-current').text
 	pc = pc.replace(',', '')
 	pc = re.search('.+\s([0-9]+).+', pc).group(1)
 
-	# discount on the product
-	dc = d = cont.find('div', 'item-info').find('div','item-action').ul.find('li','price-save').find('span','price-save-percent').text[:-1]
+	# discount on the product, if available
+	try:
+		dc = d = cont.find('div', 'item-info').find('div','item-action').ul.find('li','price-save').find('span','price-save-percent').text[:-1]
+	except:
+		dc = '0'
+	
  
 	f.write(brand + ',' + title.replace(',', ' ')
 	+ ',' + pw + ',' + pc + ',' + dc + ',' + '\n')
