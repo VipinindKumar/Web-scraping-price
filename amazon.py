0d6a6017d93fa7dcdf9995bd067c5a57b0fa12c1
VipinindKumar <Vipin.ind.Kumar@gmail.com>
2019-07-06 17:26:07 +0530

    code to extract price: previous price, current price, dicount in ₹, discount in percent for the product

--- amazon.py
+++ amazon.py
@@ -1,3 +1,5 @@
+import re
+
 productClass = 'sg-col-20-of-24 s-result-item sg-col-0-of-12 sg-col-28-of-32 sg-col-16-of-20 sg-col sg-col-32-of-36 sg-col-12-of-16 sg-col-24-of-28'
 
 def amazon(page_soup):
@@ -18,7 +20,15 @@ def amazon(page_soup):
 		rating = cont.select_one('div > div > div > div:nth-of-type(2) > div:nth-of-type(2) > div > div > div > div > div:nth-of-type(2) > div > span').get_text(strip=True)
 		
 		num_rating = cont.select_one('div > div > div > div:nth-of-type(2) > div:nth-of-type(2) > div > div > div > div > div:nth-of-type(2) > div > a').get_text(strip=True)
-
+		
+		price = cont.select_one('div > div > div > div:nth-of-type(2) > div:nth-of-type(2) > div > div:nth-of-type(2) > div').get_text(strip=True)
+		
+		price = re.search('₹(\d+,\d+)(?:\d+,\d+)(\d+,\d+)(?:\d+,\d+).+₹(\d+,\d+)\s\((\d+)%\)', price)
+		
+		pw = price.group(2).replace(',', '')
+		pc = price.group(1).replace(',', '')
+		dc = price.group(3).replace(',', '')
+		dcp = price.group(4)
 		
 		
 		f.write('')
