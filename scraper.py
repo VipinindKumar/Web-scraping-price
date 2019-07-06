4a8979b49f763676eaa25efdce80a0d57a96605d
VipinindKumar <Vipin.ind.Kumar@gmail.com>
2019-07-06 12:58:50 +0530

    update scraper with amazon url

--- scraper.py
+++ scraper.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 
 from newegg import newegg
 
-urls = ['https://www.newegg.com/global/in-en/Laptops-Notebooks/Category/ID-223?Tid=1297916'
-		]
+#urls = ['https://www.newegg.com/global/in-en/Laptops-Notebooks/Category/ID-223?Tid=1297916',
+urls = ['https://www.amazon.in/s?bbn=1375424031&rh=n%3A976392031%2Cn%3A%21976393031%2Cn%3A1375424031%2Cp_36%3A7252028031&dc&fst=as%3Aoff&qid=1562389178&rnid=7252027031']
 for url in urls:
 	# open connection to url, and download the page
 	client = urlopen(url)
@@ -19,3 +19,5 @@ for url in urls:
 		#from newegg
 		newegg(page_soup)
 
+	if 'amazon' in url:
+		amazon(page_soup)
