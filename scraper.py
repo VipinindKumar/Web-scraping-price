818c967ed06390099a678bde70bdc9039df36105
VipinindKumar <Vipin.ind.Kumar@gmail.com>
2019-07-06 13:08:31 +0530

    Add amazon.py with skelton of the function to extract data from the page

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 from newegg import newegg
+from amazon import amazon
 
 #urls = ['https://www.newegg.com/global/in-en/Laptops-Notebooks/Category/ID-223?Tid=1297916',
 urls = ['https://www.amazon.in/s?bbn=1375424031&rh=n%3A976392031%2Cn%3A%21976393031%2Cn%3A1375424031%2Cp_36%3A7252028031&dc&fst=as%3Aoff&qid=1562389178&rnid=7252027031']
