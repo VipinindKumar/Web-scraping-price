37fec0dcbe5b2096a932ba7f9c7f84e5c2d06755
VipinindKumar <Vipin.ind.Kumar@gmail.com>
2019-07-06 18:50:09 +0530

    Add try and except statements to catch errors due to unavailable attribute

--- amazon.py
+++ amazon.py
@@ -13,30 +13,39 @@ def amazon(page_soup):
 	conts = page_soup.findAll('div', {'class': productClass})
 
 	for cont in conts:
+		# get the title of the product
 		title = cont.select_one('div > div > div > div:nth-of-type(2) > div:nth-of-type(2) > div > div > div > div > div > h2 > a').get_text(strip=True)
-
+		
+		# retrieve the brand name of the product
 		brand = title.split(' ')[0]
 		
+		# star rating of the product and number of reviews, if available
 		try:
 			rating = cont.select_one('div > div > div > div:nth-of-type(2) > div:nth-of-type(2) > div > div > div > div > div:nth-of-type(2) > div > span').get_text(strip=True)
-		except:
-			rating = 'NaN'
-		
-		try:
 			num_rating = cont.select_one('div > div > div > div:nth-of-type(2) > div:nth-of-type(2) > div > div > div > div > div:nth-of-type(2) > div > a').get_text(strip=True)
 		except:
+			rating = 'NaN'
 			num_rating = 0
 		
-		pw = cont.select_one('div > div > div > div:nth-of-type(2) > div:nth-of-type(2) > div > div:nth-of-type(2) > div > div > div > div > div > a > span:nth-of-type(2) > span').get_text(strip=True)[1:].replace(',', '')
+		# previous price of the product and discount, if available
+		try:
+			# previous price
+			pw = cont.select_one('div > div > div > div:nth-of-type(2) > div:nth-of-type(2) > div > div:nth-of-type(2) > div > div > div > div > div > a > span:nth-of-type(2) > span').get_text(strip=True)[1:].replace(',', '')
+			
+			# discount in ruppee and percent
+			disc = cont.select_one('div > div > div > div:nth-of-type(2) > div:nth-of-type(2) > div > div:nth-of-type(2) > div > div > div > div > div > span:nth-of-type(2)').get_text(strip=True)
+			disc = re.search('.*₹(\d+,\d+)\s\((\d+)%\)', disc)
+			dc = disc.group(1).replace(',', '')
+			dcp = disc.group(2)
+		except:
+			pw = 'NaN'
+			dc = 0
+			dcp = 0
 		
+		# Current price of the product
 		pc = cont.select_one('div > div > div > div:nth-of-type(2) > div:nth-of-type(2) > div > div:nth-of-type(2) > div > div > div > div > div > a > span > span').get_text(strip=True)[1:].replace(',', '')
 		
-		disc = cont.select_one('div > div > div > div:nth-of-type(2) > div:nth-of-type(2) > div > div:nth-of-type(2) > div > div > div > div > div > span:nth-of-type(2)').get_text(strip=True)
-		disc = re.search('.*₹(\d+,\d+)\s\((\d+)%\)', disc)
-		dc = disc.group(1).replace(',', '')
-		dcp = disc.group(2)
-		
-		
+		# write the variables in csv file
 		f.write(brand + ',' + title.replace(',', ' ') + ',' + pw + ',' + pc + ',' + dc + ',' + dcp + ',' + rating + ',' + num_rating + '\n')
 	f.close()
 
