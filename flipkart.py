5c26e09322a95bb17352c94ea54e7f6b57a6444b
VipinindKumar <Vipin.ind.Kumar@gmail.com>
2019-07-10 18:39:37 +0530

    add code to get price, current price, previous price, discount

--- flipkart.py
+++ flipkart.py
@@ -1,3 +1,5 @@
+import re
+
 def flipkart(page_soup):
 	# create a new file in write mode
 	f = open('data/amazon-laptops.csv', 'w')
@@ -18,31 +20,37 @@ def flipkart(page_soup):
 		
 		# star rating of the product and number of reviews, if available
 		try:
-			rating = 
+			rating = cont.select_one('div > div > div > a > div:nth-of-type(3) > div > div:nth-of-type(2) > span').get_text(strip=True)
+						
+			num_rating = cont.select_one('div > div > div > a > div:nth-of-type(3) > div > div:nth-of-type(2) > span:nth-of-type(2) > span > span').get_text(strip=True)
+			num_rating = num_rating.split(' ')[0]
 			
-			num_rating = 
-			except:
+			num_review = cont.select_one('div > div > div > a > div:nth-of-type(3) > div > div:nth-of-type(2) > span:nth-of-type(2) > span > span:nth-of-type(3)').get_text(strip=True)
+			num_review = num_review.split(' ')[0]
+			
+		except:
 			rating = 'NaN'
 			num_rating = '0'
+			num_review = '0'
 		
 		# previous price of the product and discount, if available
 		try:
+			price = cont.select_one('div > div > div > a > div:nth-of-type(3) > div:nth-of-type(2) > div').get_text(strip=True)
+			price = re.search('₹(\d+,\d+)₹(\d+,\d+)(\d\d)%', price)
+			
 			# previous price
-			pw = 
-						
+			pw = price.group(2)
+			# Current price of the product
+			pc = price.group(1)	
+								
 			# discount in ruppee and percent
+			dcp = price.group(3)
 			
 		except:
 			pw = 'NaN'
+			pc = 'NaN'
 			dc = '0'
 			dcp = '0'
-		
-		try:
-			# Current price of the product
-			pc = 
-		except:
-			pc = 'NaN'
-		
 		
 		# write the variables in csv file
 		f.write(brand + ',' + title.replace(',', ' ') + ',' + pw + ',' + pc + ',' + dc + ',' + dcp + ',' + rating + ',' + num_rating + '\n')
