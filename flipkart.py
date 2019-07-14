118c0a88abd5620027996f9e9e67d491fe241eca
VipinindKumar <Vipin.ind.Kumar@gmail.com>
2019-07-14 15:59:15 +0530

    fix adding list with str

--- flipkart.py
+++ flipkart.py
@@ -96,7 +96,7 @@ def kartUrls(page_soup, url):
 	for i in range(2, last+1):
 		url = 'https://www.flipkart.com/laptops/pr?sid=6bo%2Fb5g&p%5B%5D=facets.serviceability%5B%5D%3Dtrue&fm=neo%2Fmerchandising&iid=M_3580c4f9-a714-45e8-a54c-64fa60d4b35d_10.f37da198-ab3e-48f0-bc9a-12dfbbbc32c9_DesktopSite&ppt=clp&ppn=laptops-store&page=' + str(i)
 		
-		urls = urls + url
+		urls = urls.append(url)
 	
 	return urls		
 
