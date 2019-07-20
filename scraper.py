7ee7af1bef70e900fd381e1d7def691c616c0017
VipinindKumar <Vipin.ind.Kumar@gmail.com>
2019-07-20 11:47:01 +0530

    add limit to page's urls added to scrap to limit price of the product scraped, for newegg and flipkart urls

--- scraper.py
+++ scraper.py
@@ -31,20 +31,22 @@ urls = ['https://www.newegg.com/global/in-en/Laptops-Notebooks/SubCategory/ID-32
 'https://www.flipkart.com/laptops/pr?sid=6bo%2Fb5g&p%5B%5D=facets.serviceability%5B%5D%3Dtrue&fm=neo%2Fmerchandising&iid=M_3580c4f9-a714-45e8-a54c-64fa60d4b35d_10.f37da198-ab3e-48f0-bc9a-12dfbbbc32c9_DesktopSite&ppt=clp&ppn=laptops-store&sort=price_asc']
 
 # craete urls for other pages and them to the list of pages to be parsed
-# for url in urls:
-# 	page_soup = parse(url)
+for url in urls:
+	page_soup = parse(url)
 	
-# 	if 'newegg' in url:
-# 		print('Creating URLs for newegg')
-# 		urls = urls + eggUrls(page_soup, url)
+	if 'newegg' in url:
+		print('Creating URLs for newegg')
+		# add only upto a page to prevent from going over budget
+		urls = urls + eggUrls(page_soup, url)[:25]
 	
-# 	if 'amazon' in url:
-# 		print('Creating URLs for amazon')
-# 		urls = urls + zonUrls(page_soup, url)
+	if 'amazon' in url:
+		print('Creating URLs for amazon')
+		urls = urls + zonUrls(page_soup, url)
 
-# 	if 'flipkart' in url:
-# 		print('Creating URLs for flipkart')
-# 		urls = urls + kartUrls(page_soup, url)
+	if 'flipkart' in url:
+		print('Creating URLs for flipkart')
+		# add only upto a page to prevent from going over budget
+		urls = urls + kartUrls(page_soup, url)[:10]
 
 
 # extract product's information and write them in csv files
