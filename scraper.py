f7582bc4e585fbd9ed80bf455aeb207a0ce1927d
VipinindKumar <Vipin.ind.Kumar@gmail.com>
2019-07-20 11:40:47 +0530

    change flipkart url to have listing sorted from low price to high price

--- scraper.py
+++ scraper.py
@@ -28,7 +28,7 @@ urls = ['https://www.newegg.com/global/in-en/Laptops-Notebooks/SubCategory/ID-32
 # amazon 20k to 30k
 'https://www.amazon.in/s?bbn=1375424031&rh=n%3A976392031%2Cn%3A976393031%2Cn%3A1375424031%2Cp_36%3A2000000-3000000&s=price-asc-rank', 
 
-'https://www.flipkart.com/laptops/pr?sid=6bo%2Fb5g&p%5B%5D=facets.serviceability%5B%5D%3Dtrue&fm=neo%2Fmerchandising&iid=M_3580c4f9-a714-45e8-a54c-64fa60d4b35d_10.f37da198-ab3e-48f0-bc9a-12dfbbbc32c9_DesktopSite&ppt=clp&ppn=laptops-store']
+'https://www.flipkart.com/laptops/pr?sid=6bo%2Fb5g&p%5B%5D=facets.serviceability%5B%5D%3Dtrue&fm=neo%2Fmerchandising&iid=M_3580c4f9-a714-45e8-a54c-64fa60d4b35d_10.f37da198-ab3e-48f0-bc9a-12dfbbbc32c9_DesktopSite&ppt=clp&ppn=laptops-store&sort=price_asc']
 
 # craete urls for other pages and them to the list of pages to be parsed
 # for url in urls:
